3062bd4f2cae23654161de6c0396b77851e923d9
sergey-komar <kursev81@yandex.ru>
2024-10-30 16:18:36 +0300

    heightvh

--- app/js/main.js
+++ app/js/main.js
@@ -56,6 +56,10 @@ window.addEventListener('DOMContentLoaded', () => {
     }
    
 
+    let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 })
    
    
